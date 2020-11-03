4cba5b20af7e49cf28c9fe845d4f5119a7173a59
Laila Akiyoshi <52533670+lailaakiyoshi@users.noreply.github.com>
2020-11-03 20:13:33 -0300

    Version 001
    - Initiation of the API with Spring Boot framework

---
+++ src/main/java/com/br/apirestspringboot/ApiRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.br.apirestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootApplication.class, args);
+	}
+
+}
